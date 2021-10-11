f104ef2c5aa674a8bea1a25029196b395004225a
nagarizki <akuakusaya@gmail.com>
2021-10-11 23:52:07 +0700

    rsvp functionality

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Pernikahan Mei dan Nagari",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Oct 16, 2021 11:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Oct 16, 2021 14:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Gedung Paroki, Kutoarjo',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Kami menunggu kehadiran Bapak/Ibu/Saudara/i. Apabila ada pertanyaan dapat menghubungi Meidika di nomor +62 85725092099."
         }
     });
 
@@ -214,11 +214,11 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== '23853c45eff4474134f60834643b1490'
+            && MD5($('#invite_code').val()) !== '23853c45eff4474134f60834643b1490') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzdu0E81KBRkm4oZjA6ta63SEKiGozseeokNtN6vkMtT72JrcB4muyQMmImcjLxuxSh0Q-/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
